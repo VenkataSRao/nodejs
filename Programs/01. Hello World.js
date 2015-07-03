5c2ce8b93818ed56916ff559a3f980eb9db52e4d
VenkataSRao <vraoblr@gmail.com>
2015-07-03 14:27:12 +0530

    Hello world basic program

---
+++ Programs/01. Hello World.js
@@ -0,0 +1,14 @@
+This program will give just hello word ..
+
+after installing node js and execute this program
+
+HelloWorld.js
+===========
+
+console.log("Hello Welcome to Node World");
+
+
+Output:
+======
+
+Hello Welcome to Node World
